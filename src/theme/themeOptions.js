139742783ce0f19c8866a92ee54a614733b95362
jennahakulinen <jenna.hakulinen@metropolia.fi>
2022-04-24 17:58:57 +0300

    teeman värit

--- src/theme/themeOptions.js
+++ src/theme/themeOptions.js
@@ -1,20 +1,35 @@
+// import Fredoka from './fonts/FredokaOne-Regular.ttf';
+
 const themeOptions = {
   palette: {
-    type: 'light',
     primary: {
-      main: '#00104a',
+      main: '#76CFDB',
     },
+    primaryVariant: {
+      main: '#48A0B3',
+    },
+    // tää on se beessi, täytyy kattoo joku muu väri!
     secondary: {
-      main: '#d60202',
+      main: '#D0AE8B',
+    },
+    secondaryVariant: {
+      main: '#F5F2EB',
+    },
+    backgroundColor: {
+      main: '#F7F7F7',
+    },
+    bodyTextColor: {
+      main: '#05192C',
+    },
+    heart: {
+      main: '#FF2222',
     },
   },
   typography: {
-    fontFamily: 'Lato',
-    body1: {
-      fontFamily: 'Droid Sans',
-    },
-    body2: {
-      fontFamily: 'Droid Sans',
+    fontFamily: 'Fredoka',
+    h1: {
+      fontFamily: 'Comic Sans MS',
+      fontSize: '46px',
     },
   },
   shape: {
